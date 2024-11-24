cea02488361a712d8e314e03492931e3f84505e4
Kborate123 <komal.borate@d2kindia.com>
2024-11-25 04:34:03 +0530

    add-weather-analysis-data-pipeline

--- dagster_weather/processing_weather/processing_weather/assets/__init__.py
+++ dagster_weather/processing_weather/processing_weather/assets/__init__.py
@@ -3,6 +3,6 @@ from . import analysis_data
 
 
 ANALYSIS_DATA = "analysis_data"
-stock_price_assets = load_assets_from_package_module(
+weather_analysis_data_assets = load_assets_from_package_module(
     package_module=analysis_data, group_name=ANALYSIS_DATA
 )
